a63600b28653c4319bfa2c2c1a56af0cdbc7c2d6
Ari de Arriba <aridease@gmail.com>
2020-10-28 11:11:27 +0100

    Factory pattern for translators

--- src/main/java/com/api/sentimentanalysis/externalapi/translator/TranslatorAPI.java
+++ src/main/java/com/api/sentimentanalysis/externalapi/translator/TranslatorAPI.java
@@ -1,6 +1,22 @@
 package com.api.sentimentanalysis.externalapi.translator;
 
+import org.json.simple.parser.ParseException;
+import java.io.IOException;
+
+/** Interface for translator in factory pattern.
+ *
+ * @author Ariadna de Arriba
+ */
 public interface TranslatorAPI
 {
-    String translate(String text, String lang_src, String lang_dest) throws Exception;
+    /** Make a request to translator API to translate a text.
+     *
+     * @param text Text to translate.
+     * @param lang_src Language of the original text. For example: 'en'.
+     * @param lang_dest Language to translate the text. For example: 'es'.
+     * @return Returns a string which represents a json with the translated text.
+     * @throws ParseException {@link ParseException caused parsing the json.}
+     * @throws IOException {@link IOException caused by an error in the API call. }
+     */
+    String translate(String text, String lang_src, String lang_dest) throws IOException, ParseException;
 }
--- src/main/java/com/api/sentimentanalysis/externalapi/translator/TranslatorAPIFactory.java
+++ src/main/java/com/api/sentimentanalysis/externalapi/translator/TranslatorAPIFactory.java
@@ -2,9 +2,15 @@ package com.api.sentimentanalysis.externalapi.translator;
 
 public class TranslatorAPIFactory
 {
-    public static TranslatorAPI getTranslatorAPI(String api, String apiKey)
+    /** Factory pattern to instantiate a translator class.
+     *
+     * @param translator Translator API.
+     * @param apiKey Api-key to authorize the method.
+     * @return Returns an instance of the translator class.
+     */
+    public static TranslatorAPI getTranslatorAPI(String translator, String apiKey)
     {
-        if ("Microsoft".equalsIgnoreCase(api)) return new Microsoft(apiKey);
+        if ("Microsoft".equalsIgnoreCase(translator)) return new Microsoft(apiKey);
         throw new NullPointerException("No api with this name");
     }
 }
