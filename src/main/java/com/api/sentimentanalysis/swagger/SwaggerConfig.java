8152446e89ab8db6076c25a62f412ef395c6b598
Ari de Arriba <aridease@gmail.com>
2020-10-28 11:12:13 +0100

    Swagger config

--- src/main/java/com/api/sentimentanalysis/swagger/SwaggerConfig.java
+++ src/main/java/com/api/sentimentanalysis/swagger/SwaggerConfig.java
@@ -7,17 +7,21 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Arrays;
-
+/** Swagger configuration class.
+ *
+ */
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig
 {
+    /** Method to configure swagger.
+     *
+     * @return Returns a Docket.
+     */
     @Bean
     public Docket api()
     {
@@ -30,6 +34,10 @@ public class SwaggerConfig
                 .apiInfo(apiInfo());
     }
 
+    /** Method to configure API information.
+     *
+     * @return Returns an instance of ApiInfo.
+     */
     private ApiInfo apiInfo()
     {
         return new ApiInfoBuilder()
