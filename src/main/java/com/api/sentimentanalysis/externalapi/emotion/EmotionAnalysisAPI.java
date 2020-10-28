9f92dcc2477b904003205cedc5366f88614c76de
Ari de Arriba <aridease@gmail.com>
2020-10-28 11:10:14 +0100

    Factory pattern for machine learning tools

--- src/main/java/com/api/sentimentanalysis/externalapi/emotion/EmotionAnalysisAPI.java
+++ src/main/java/com/api/sentimentanalysis/externalapi/emotion/EmotionAnalysisAPI.java
@@ -1,6 +1,19 @@
 package com.api.sentimentanalysis.externalapi.emotion;
 
+import java.io.IOException;
+
+/** Interface for getEmotion analysis in factory pattern.
+ *
+ * @author Ariadna de Arriba
+ */
 public interface EmotionAnalysisAPI
 {
-    String emotion(String text) throws Exception;
+    /** Make a request to ML tool for sentiment analysis.
+     *
+     * @param text Text to analyze.
+     * @return Returns a string that contains a json with each of six emotions weighted.
+     * @throws NullPointerException {@link NullPointerException caused by an error during a call to the API and returns nothing.}
+     * @throws IOException {@link IOException caused by an error in the API call. }
+     */
+    String getEmotion(String text) throws NullPointerException, IOException;
 }
--- src/main/java/com/api/sentimentanalysis/externalapi/emotion/EmotionAnalysisAPIFactory.java
+++ src/main/java/com/api/sentimentanalysis/externalapi/emotion/EmotionAnalysisAPIFactory.java
@@ -1,11 +1,22 @@
 package com.api.sentimentanalysis.externalapi.emotion;
 
+/** Class to instantiate a machine learning tool class in factory pattern.
+ *
+ * @author Ariadna de Arriba
+ */
 public class EmotionAnalysisAPIFactory
 {
-    public static EmotionAnalysisAPI getEmotionAnalysisAPI(String api, String apiKey)
+    /** Factory pattern to instantiate a machine learning tool class.
+     *
+     * @param tool Machine learning tool.
+     * @param apiKey Api-key to authorize the method.
+     * @return Returns an instance of the ML tool class.
+     */
+    public static EmotionAnalysisAPI getEmotionAnalysisAPI(String tool, String apiKey)
     {
-        if ("ParallelDots".equalsIgnoreCase(api)) return new ParallelDots(apiKey);
-        if ("BERT".equalsIgnoreCase(api)) return new BERT(apiKey);
+        if ("ParallelDots".equalsIgnoreCase(tool)) return new ParallelDots(apiKey);
+        if ("BERT".equalsIgnoreCase(tool)) return new BERT(apiKey);
+        if ("SVC".equalsIgnoreCase(tool)) return new SVC(apiKey);
         throw new NullPointerException("No api with this name");
     }
 }
